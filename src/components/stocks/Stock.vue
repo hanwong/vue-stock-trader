13878fc303daabefa6237faaf9c33553f311e75f
Kim Han Woong <hanationbear@gmail.com>
2017-04-08 00:43:50 +0900

    Adding Buy Button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    <h1>Stocks - Stock</h1>
+  <div class="col-md-4 col-sm-6">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} )</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input 
+              type="number"
+              class="form-control"
+              placeholder="Quantity"
+              v-model.number="quantity"
+          >
+        </div>
+        <div class="pull-right">
+          <button
+              class="btn btn-success"
+              @click="buyStock"
+              :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+              >Buy</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    props: ['stock'],
+    
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity,
+        };
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+
+  }
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <h1>Stocks</h1>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
   </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BENZ', price: 200},
+          { id: 2, name: 'AUDI', price: 150},
+          { id: 3, name: 'HONDA', price: 180},
+          { id: 4, name: 'LEXUS', price: 195}
+        ]
+      }
+    },
+
+    components: {
+      appStock: Stock
+    }
+
+  }
+</script>
