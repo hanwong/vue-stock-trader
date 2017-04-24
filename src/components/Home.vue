1d67e81ba79963463ae2502c40f86c3a542ea5b0
Kim Han Woong <hanationbear@gmail.com>
2017-04-24 22:45:27 +0900

    Adding Filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
     <h6>You may Save & Load your Data</h6>
     <h6>Click on 'End Day' to begin a New Day!</h6>
     <hr>
-    <p>Your Funds: {{ funds }}</p>
+    <p>Your Funds: {{ funds | currency }}</p>
   </div>
 </template>
 
